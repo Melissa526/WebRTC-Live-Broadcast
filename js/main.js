b17333a787f2ba3678d3f05ee3561e3065dcc280
Melissa526 <lotus526@naver.com>
2019-08-09 23:29:04 +0900

    190809-2328

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* Socket Side */
+$(document).ready(function(){
+    var socket = io()
+    var roomNum
+    var userName
+
+    $(document).on('click', '.startButton', () => {
+        roomNum = parseInt(Math.random()*9999999999)
+        userName = $('#userName').val()
+        $('#userName').val('')
+        socket.emit('create', roomNum, userName)
+    }).on('click', '.showLive', () => {
+        var addr = $(this).siblings('input[name=addr]').val()
+        //socket.emit('join', addr)
+        console.log($(this))
+        console.log(addr);
+        
+    })
+
+    socket.on('roomList', (list) => {
+        $('.channel')
+        .append($('<div class="channelBox">')
+        .append(`<h1 class="h1-title"><a class="showLive" href="#">${roomNum}</a></h1>
+            caster : <h5>${userName}</h5>
+            <input type="hidden" name="addr" value="${roomNum}">
+            </div>`))
+    })
+
+})
